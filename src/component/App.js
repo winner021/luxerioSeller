f969db831e223389fb6c0db10d8682033b948b36
winner021 <90254653+winner021@users.noreply.github.com>
2022-06-21 16:00:54 +0530

    first commit

--- src/component/App.js
+++ src/component/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux';
 
 import Addproduct from './products/Addproduct'
-import Deleteproduct from "./products/Deleteproduct"
+
 import Editproduct from "./products/Editproduct"
-import Showproduct from "./products/Showproduct"
+
 import { BrowserRouter,Route } from 'react-router-dom/cjs/react-router-dom.min'
 import Header from "./Header"
 const App=()=>{
@@ -13,9 +13,9 @@ return(
       <BrowserRouter>
       <Header/>
         <Route path="/" exact component={Addproduct}/>
-        <Route path="/delete" exact component={Deleteproduct}/>
+        
         <Route path="/edit" exact component={Editproduct}/>
-        <Route path="/show" exact component={Showproduct}/>
+        
       </BrowserRouter>
     </div>
 )
